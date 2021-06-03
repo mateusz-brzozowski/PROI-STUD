6c576240c3c5f27731b47d4f2e22285bb50f5b51
Mikołaj Kuranowski <mkuranowski@gmail.com>
2021-06-03 16:39:05 +0200

    Car: De-duplicate collistion detection; add docstrings

--- Car.cpp
+++ Car.cpp
@@ -13,17 +13,30 @@
 
 void Car::set_map(Map* map) { m_map = map; }
 
-void Car::clamp_position() {
-    // Clamp the values
+bool Car::validate_new_position(Vector2D new_pos) {
+    // Clamp within Map bounds
     double w = m_map->get_window().get_width() - 18;
     double h = m_map->get_window().get_height() - 18;
 
-    m_position.x = CLAMP(m_position.x, 0, w);
-    m_position.y = CLAMP(m_position.y, 0, h);
+    new_pos.x = CLAMP(new_pos.x, 0, w);
+    new_pos.y = CLAMP(new_pos.y, 0, h);
 
-    // Update the texture position
-    m_texture_position.x = (int)m_position.x;
-    m_texture_position.y = (int)m_position.y;
+    // Check for collisions
+    SDL_Rect new_texture_pos = m_texture_position;
+    new_texture_pos.x = new_pos.x;
+    new_texture_pos.y = new_pos.y;
+
+    for (auto& e : m_map->get_objects())
+        if (*e != *this &&
+            CollisionAABB(e->get_texture_position(), &new_texture_pos)) {
+            // Collision with a different object detected
+            // - don't update the position
+            return false;
+        }
+
+    m_position = new_pos;
+    m_texture_position = new_texture_pos;
+    return true;
 }
 
 void Car::update() {
@@ -34,17 +47,8 @@ void Car::update() {
     // Then move the car in the direction pointed by angle
     double distance = (m_map->get_window().is_pressed(SDLK_w) ? 5.0 : 0.0) -
                       (m_map->get_window().is_pressed(SDLK_s) ? 5.0 : 0.0);
-    auto old_pos = m_position;
-    m_position +=  Vector2D{distance, 0}.rotate(m_angle);
-    clamp_position();
-    for (auto &e : m_map->get_objects()) {
-        if (*e != *this &&
-            CollisionAABB(get_texture_position(), e->get_texture_position())) {
-            m_position = old_pos;
-            clamp_position();
-            break;
-        }
-    }
+
+    validate_new_position(m_position + Vector2D{distance, 0}.rotate(m_angle));
 }
 
 SDL_Texture* Car::get_texture() {
@@ -64,21 +68,9 @@ double Car::get_texture_rotation() { return m_angle * 180.0 / M_PI; }
 void AutonomousCar::update() {
     if (m_follow_car->get_position().distance(m_position) > 30) {
         Vector2D direction = m_follow_car->get_position() - m_position;
-        float angle = atan2(direction.y, direction.x);
+        m_angle = atan2(direction.y, direction.x);
         direction.normalize();
-
-        m_angle = angle;
-        auto old_pos = m_position;
-        m_position += (direction * (m_speed * 1 / 60));
-        clamp_position();
-        for (auto& e : m_map->get_objects()) {
-            if (*e != *this && CollisionAABB(get_texture_position(),
-                                             e->get_texture_position())) {
-                m_position = old_pos;
-                clamp_position();
-                break;
-            }
-        }
+        validate_new_position(m_position + (direction * (m_speed * 1 / 60)));
     }
 }
 
--- Car.h
+++ Car.h
@@ -10,14 +10,30 @@
  */
 class Car : public IMapObject {
    protected:
-    SDL_Texture* m_texture{NULL};
+    /// Texture of that car
+    SDL_Texture* m_texture{nullptr};
+
+    /// Position of this car's texture
     SDL_Rect m_texture_position{0, 0, 0, 0};
 
+    /// Position of the car
     Vector2D m_position{0.0, 0.0};
+
+    /// Angle of the car
     double m_angle{0.0};
+
+    /// A pointer to the connected map
     Map* m_map{nullptr};
 
-    void clamp_position();
+    /**
+     * validate_new_position() first checks if the new position is within the
+     * bound of the Map, then checks if the new position collides with any other
+     * object on the Map, and then finally updates the m_texture_position.
+     *
+     * @returns true if the new position was deemed valid and the Car was
+     * actually moved
+     */
+    bool validate_new_position(Vector2D);
 
    public:
     Car(Vector2D start_position) : m_position(start_position) {}
@@ -30,9 +46,16 @@ class Car : public IMapObject {
     double get_texture_rotation() override;
 };
 
+/**
+ * AutonomousCar is an extension to the Car class,
+ * that provides an autonomous car, which follows another Car.
+ */
 class AutonomousCar : public Car {
    protected:
+    /// The car to follow
     Car* m_follow_car;
+
+    /// Max allowed speed of this autonomous car
     double m_speed;
 
    public:
